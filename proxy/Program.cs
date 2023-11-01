2f209d255f76cea162742e00d00f75dbac0461c3
Gilberto Ferreira <gilberto@maess.com.br>
2023-10-31 23:12:07 -0300

    proxy auth com yarp e keycloak

---
+++ proxy/Program.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.IdentityModel.Logging;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+builder.Services
+    .AddAuthentication(cfg =>
+    {
+        cfg.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        cfg.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+    })
+    .AddOpenIdConnect(cfg =>
+    {
+        cfg.Authority = builder.Configuration["SSO_AUTHORITY"];
+        cfg.ClientId = builder.Configuration["SSO_CLIENT_ID"];
+        cfg.ClientSecret = builder.Configuration["SSO_CLIENT_SECRET"];
+        cfg.UsePkce = true;
+        cfg.RequireHttpsMetadata = false;
+
+        cfg.Scope.Clear();
+        cfg.Scope.Add("openid");
+        cfg.Scope.Add("profile");
+    });
+    
+builder.Services.Configure<CookiePolicyOptions>(options =>
+{
+    options.CheckConsentNeeded = context => false;
+    options.MinimumSameSitePolicy = SameSiteMode.None;
+});
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("customPolicy", policy =>
+        policy.RequireAuthenticatedUser());
+});
+
+var app = builder.Build();
+IdentityModelEventSource.ShowPII = true;
+var options = new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+};
+options.KnownNetworks.Clear();
+options.KnownProxies.Clear();
+app.UseForwardedHeaders(options);
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapReverseProxy();
+});
+app.Run();
